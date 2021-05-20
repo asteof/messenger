19ca37d6e935b21e028a3301c354c337ed6ebab1
asteof <kirill.brezhnev02@gmail.com>
2021-05-20 17:27:06 +0300

    Changed API URL to production (bug fixed v2)

--- src/components/constants/API_PATH_DEFAULT.js
+++ src/components/constants/API_PATH_DEFAULT.js
@@ -1,4 +1,4 @@
-const API_PATH = 'https://legendary-bastard.herokuapp.com/'
+const API_PATH = 'https://legendary-bastard.herokuapp.com'
 const SECURED_API_PATH = 'https://legendary-bastard.herokuapp.com/api/v1'
 
 export {API_PATH, SECURED_API_PATH};
