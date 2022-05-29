1cf897808843a4b81ded2c522d36186102322a26
asteof <kirill.brezhnev02@gmail.com>
2022-05-29 19:54:57 +0300

    bugfix

--- src/constants/API_PATH_DEFAULT.js
+++ src/constants/API_PATH_DEFAULT.js
@@ -2,7 +2,7 @@
 // const API_PATH = 'http://localhost:8080'
 // const SECURED_API_PATH = 'http://localhost:8080/api/v1'
 const API_PATH = 'https://cheese-giant.herokuapp.com'
-const SECURED_API_PATH = 'https://cheese-giant.herokuapp.com'
+const SECURED_API_PATH = 'https://cheese-giant.herokuapp.com/api/v1'
 // prod
 
 export {API_PATH, SECURED_API_PATH};
