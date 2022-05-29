4c9748cf7239aa2004239f687bda1b4ebb59ce46
asteof <kirill.brezhnev02@gmail.com>
2022-05-29 15:35:11 +0300

    bugfix

--- src/constants/API_PATH_DEFAULT.js
+++ src/constants/API_PATH_DEFAULT.js
@@ -1,7 +1,7 @@
 //dev
 // const API_PATH = 'http://localhost:8080'
 // const SECURED_API_PATH = 'http://localhost:8080/api/v1'
-const API_PATH = 'http://34.233.31.56/'
+const API_PATH = 'http://34.233.31.56'
 const SECURED_API_PATH = 'http://34.233.31.56/api/v1'
 // prod
 
